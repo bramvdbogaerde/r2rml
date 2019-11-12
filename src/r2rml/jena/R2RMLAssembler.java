3599b81fb5c5a4339afaec386524faeb8fb2dd83
Maxim Van de Wynckel <maxim@mvdw-software.com>
2019-11-12 11:21:17 +0100

    Bugfix: new model so the data does not get added to the TDB dataset, Added documentation to class

--- src/r2rml/jena/R2RMLAssembler.java
+++ src/r2rml/jena/R2RMLAssembler.java
@@ -13,7 +13,39 @@ import java.util.logging.Logger;
 
 /**
  * R2RML Model Assembler
- *  Processes R2RML mapping and adds the output triples to the base model
+ * <p>
+ *      Processes R2RML mapping and adds the output triples to the base model
+ *      that is configured in the assembler configuration.
+ * </p>
+ *
+ * Add the prefix to the top of the assembler configuration (/etc/fuseki/configuration/*.ttl)
+ * This prefix matches the URI used below in the code
+ * <pre>
+ *     {@literal @}prefix r2rml: &lt;http://r2rml#&gt;.
+ * </pre>
+ *
+ * Load this class and specify that the r2rml:Model is a sub class of a ja:NamedModel
+ * <pre>
+ *     [] ja:loadClass "r2rml.jena.R2RMLAssembler" .
+ *     r2rml:Model rdfs:subClassOf ja:NamedModel .
+ * </pre>
+ *
+ * Create a graph
+ * The r2rml:baseModel is the graph that will be used to add the new triples to
+ * The r2rml:connectionURL is the JDBC URL (relative to the launch path of the web container)
+ * The r2rml:mappingFile is the r2rml mapping file (relative to the launch path of the web container)
+ * <pre>
+ *      :r2rml_dataset a ja:RDFDataset ;
+ * 		        ja:defaultGraph :r2rml_model .
+ *      :r2rml_model a r2rml:Model ;
+ * 		        r2rml:baseModel :tdb_graph ;
+ * 		        r2rml:connectionURL "jdbc:sqlite:D:/OIS/cinema.db" ;
+ * 		        r2rml:mappingFile "D:/OIS/cinema-mapping.ttl" .
+ *
+ *      # Create a graph for the tdb_dataset.
+ *      :tdb_graph a tdb:GraphTDB ;
+ * 		        tdb:dataset :tdb_dataset_readwrite .
+ * </pre>
  *
  * @author Maxim Van de Wynckel (mvdewync@vub.be)
  */
@@ -51,8 +83,12 @@ public class R2RMLAssembler extends ModelAssembler {
             throw new AssemblerException( root, "No r2rml:baseModel specified!");
         }
         Model baseModel = a.openModel(rootModel, Mode.ANY);
+        // Create a new model (do not extend on the base model, else you would add it to the persistent database)
+        Model newModel = ModelFactory.createDefaultModel();
+        newModel.add(baseModel);
 
         // Get the configuration from the root resource
+        Configuration configuration = new Configuration();
         Literal confMappingFile = getUniqueLiteral(root, ResourceFactory.createProperty(URI + "mappingFile"));
         if (confMappingFile == null){
             throw new AssemblerException( root, "No r2rml:mappingFile specified!");
@@ -61,17 +97,24 @@ public class R2RMLAssembler extends ModelAssembler {
         if (confConnectionURL == null){
             throw new AssemblerException( root, "No r2rml:connectionURL specified!");
         }
-
-        // Run the R2RML processor
-        Configuration configuration = new Configuration();
+        Literal confUser = getUniqueLiteral(root, ResourceFactory.createProperty(URI + "user"));
+        if (confUser != null){
+            configuration.setUser(confUser.getString());
+        }
+        Literal confPassword = getUniqueLiteral(root, ResourceFactory.createProperty(URI + "password"));
+        if (confPassword != null){
+            configuration.setPassword(confPassword.getString());
+        }
         configuration.setMappingFile(confMappingFile.getString());
         configuration.setConnectionURL(confConnectionURL.getString());
+
+        // Run the R2RML processor
         R2RMLProcessor engine = new R2RMLProcessor(configuration);
         engine.execute();
 
         // Add the data model (R2RML processed results) to the base model
-        baseModel.add(engine.getDataset().getDefaultModel());
-        return baseModel;
+        newModel.add(engine.getDataset().getDefaultModel());
+        return newModel;
     }
 
     @Override
